b74787f2fcbe71a7160ed167fc0a79020b9ceb6d
Dexter <gusevsergey85@gmail.com>
2022-12-10 11:35:30 +0300

    Заполнение структуры HashMap

--- src/main/java/org/example/SortStaff.java
+++ src/main/java/org/example/SortStaff.java
@@ -1,4 +1,9 @@
 package org.example;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 /*
 Пусть дан список сотрудников:Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, Иван Юрин,
 Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина, Мария Рыкова, Марина Лугова,
@@ -7,4 +12,29 @@ package org.example;
  Отсортировать по убыванию популярности.
  */
 public class SortStaff {
+    public static void main(String[] args){
+        String[] staff = {"Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина", "Анна Крутова", "Иван Юрин",
+                "Петр Лыков", "Павел Чернов", "Петр Чернышов", "Мария Федорова", "Марина Светлова", "Мария Савина", "Мария Рыкова", "Марина Лугова",
+                "Анна Владимирова", "Иван Мечников", "Петр Петин", "Иван Ежов"};
+        ArrayList<String> names = new ArrayList<>();
+        for (String item: staff) {
+            var data = List.of(item.split(" "));
+            names.add(data.get(0));
+        }
+        HashMap<String, Integer> countNames = new HashMap<>();
+        for (String name: names) {
+            countNames.put(name, Collections.frequency(names, name));
+        }
+        Map<String, Integer> sortedCountNames =  countNames.entrySet().stream().sorted(Comparator.comparingInt(e->-e.getValue())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> {throw new AssertionError();}, LinkedHashMap::new));
+        for (Map.Entry entry: sortedCountNames.entrySet()) {
+            if(!(entry.getValue().equals(1))){
+                System.out.println(entry.getKey() +" "+entry.getValue());
+            }
+        }
+
+
+    }
+
+
+
 }
--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -1,38 +1,35 @@
 package org.example;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 // Реализуйте структуру телефонной книги с помощью HashMap,
 // учитывая, что 1 человек может иметь несколько телефонов.
 public class Task1 {
     public static void main(String[] args) {
-        List<String> names = List.of(new String[]{"Зезюлин Дмитрий", "Боюр Николай", "Горюнова Евгения",
+        String[] names = {"Зезюлин Дмитрий", "Боюр Николай", "Горюнова Евгения",
                 "Ведентьева Елена", "Немова Анжелика", "Пармехина Элина", "Иннокентьев Марсель", "Чубыкин Петр",
-                "Бобринская Жанна", "Бекишева Виктория"});
-        List<String> homePhones = List.of(new String[]{"+7(181)438855", "null", "+7(45)522574", "+7(819)976568",
-                "null", "+7(3898)248796", "null", "null", "+7(6074)427203", "null"});
-        List<String> workingPhones = List.of(new String[]{"+7(703)461659", "+7(86)456076", "null", "+7(248)947606",
-                "null", "+7(606)812337", "null", "+7(4053)189616", "+7(845)792512", "+7(88)518282"});
-        List<String> mobilePhones = List.of(new String[]{"+7(909)662-81-8577", "+7(909)124-41-6698", "+7(909)897-09-2720",
+                "Бобринская Жанна", "Бекишева Виктория"};
+        String[] homePhones = {"+7(181)438855", "null", "+7(45)522574", "+7(819)976568",
+                "null", "+7(3898)248796", "null", "null", "+7(6074)427203", "null"};
+        String[] workingPhones = {"+7(703)461659", "+7(86)456076", "null", "+7(248)947606",
+                "null", "+7(606)812337", "null", "+7(4053)189616", "+7(845)792512", "+7(88)518282"};
+        String[] mobilePhones = {"+7(909)662-81-8577", "+7(909)124-41-6698", "+7(909)897-09-2720",
                 "+7(909)877-05-8915", "+7(909)669-00-0679", "+7(909)784-58-3766", "+7(909)744-45-0342",
-                "+7(909)184-09-5163", "+7(909)109-55-8935", "+7(909)020-47-5500"});
-        HashMap<String, HashMap<String, String>> phoneBook= new HashMap<>();
-        HashMap<String, String> phones = new HashMap<>();
-        for (int i = 0; i < mobilePhones.size(); i++) {
-            phones.put("Домашний", homePhones.get(i));
-            phones.put("Мобильный", mobilePhones.get(i));
-            phones.put("Рабочий", workingPhones.get(i));
-            phoneBook.put(names.get(i), phones);
+                "+7(909)184-09-5163", "+7(909)109-55-8935", "+7(909)020-47-5500"};
+        Map<String, Map<String, List<String>>> phoneBook = new HashMap<>();
+        for (int i = 0; i < mobilePhones.length; i++) {
+            Map<String, List<String>> phoneList= new HashMap<>();
+            phoneList.put("work", List.of(workingPhones[i]));
+            phoneList.put("mobile", List.of(mobilePhones[i]));
+            phoneList.put("home", List.of(homePhones[i]));
+            phoneBook.put(names[i], phoneList);
         }
+        for (Map.Entry entry: phoneBook.entrySet()) {
+                System.out.println(entry.getKey() +" "+entry.getValue());
+            }
 
-        for (String key: phoneBook.keySet())
-        {
-            System.out.println(key + " --> " + phoneBook.get(key));
-        }
+
 
 
 
